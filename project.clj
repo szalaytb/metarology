654f23f3024eedf628fea1e29b285a27f17e9094
szalaytb <core966@yahoo.com>
2021-08-28 12:31:11 +0200

    Initial commit

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojure-getting-started "1.0.0-SNAPSHOT"
+(defproject clojure-metarology "1.0.1-SNAPSHOT"
   :description "Demo Clojure web app"
   :url "http://clojure-getting-started.herokuapp.com"
   :license {:name "Eclipse Public License v1.0"
@@ -7,8 +7,9 @@
                  [compojure "1.6.1"]
                  [ring/ring-jetty-adapter "1.7.1"]
                  [environ "1.1.0"]]
+  :repl-options{:timeout 240000}
   :min-lein-version "2.0.0"
   :plugins [[environ/environ.lein "0.3.1"]]
   :hooks [environ.leiningen.hooks]
-  :uberjar-name "clojure-getting-started-standalone.jar"
+  :uberjar-name "clojure-metarology-standalone.jar"
   :profiles {:production {:env {:production true}}})
